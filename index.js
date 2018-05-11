99571df7cd8a6885f1303e53386027170232625f
Tobias Engelberg <tobias.engelberg@enerxess.com>
2018-05-11 10:04:32 +0200

    Modified index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const merlotExpress = require('./dist/merlot-express-middleware');
 const merlotMongoose = require('./dist/merlot-mongoose');
 const merlotTypes = require('./dist/merlot-types');
 
-module.exports = {
-  core: merlot,
-  express: merlotExpress,
-  mongoose: merlotMongoose,
-  types: merlotTypes
-};
+const exportObj = merlot;
+exportObj.express = merlotExpress.default;
+exportObj.mongoose = merlotMongoose.default;
+exportObj.types = merlotTypes.default;
+
+module.exports = exportObj;
